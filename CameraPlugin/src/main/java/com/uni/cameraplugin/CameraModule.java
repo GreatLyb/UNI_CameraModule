7d5c4e6a1e5ae037fc905e3485e3baa8c91b3c55
李亚彪 <1778934152@qq.com>
2023-03-19 11:04:07 +0800

    跳转到原生界面

--- CameraPlugin/src/main/java/com/uni/cameraplugin/CameraModule.java
+++ CameraPlugin/src/main/java/com/uni/cameraplugin/CameraModule.java
@@ -1,7 +1,11 @@
 package com.uni.cameraplugin;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.util.Log;
 
+
+import com.uni.cameraplugin.activity.DemoMainActivity;
 
 import io.dcloud.feature.uniapp.annotation.UniJSMethod;
 import io.dcloud.feature.uniapp.bridge.UniJSCallback;
@@ -9,6 +13,7 @@ import io.dcloud.feature.uniapp.common.UniModule;
 
 public class CameraModule extends UniModule {
     private static final String TAG = "CameraModule";
+    public static int REQUEST_CODE = 1000;
 
     //run ui thread
     @UniJSMethod(uiThread = true)
@@ -16,6 +21,10 @@ public class CameraModule extends UniModule {
         Log.e(TAG, "openCamera--");
         if (callback != null) {
 //            callback.invoke(data);
+        }
+        if(mUniSDKInstance != null && mUniSDKInstance.getContext() instanceof Activity) {
+            Intent intent = new Intent(mUniSDKInstance.getContext(), DemoMainActivity.class);
+            ((Activity)mUniSDKInstance.getContext()).startActivityForResult(intent, REQUEST_CODE);
         }
     }
 
