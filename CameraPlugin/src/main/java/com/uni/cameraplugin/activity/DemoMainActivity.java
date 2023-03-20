47ebceb10ef2676df1ac132b354910853b322bdc
李亚彪 <1778934152@qq.com>
2023-03-20 17:58:31 +0800

    去除返回点击逻辑判断 新增签名文件

--- CameraPlugin/src/main/java/com/uni/cameraplugin/activity/DemoMainActivity.java
+++ CameraPlugin/src/main/java/com/uni/cameraplugin/activity/DemoMainActivity.java
@@ -849,17 +849,17 @@ public class DemoMainActivity extends AppCompatActivity implements View.OnClickL
 
     private long mPressedTime = 0;
 
-    @Override
-    public void onBackPressed() {
-        long mNowTime = System.currentTimeMillis();//获取第一次按键时间
-        if ((mNowTime - mPressedTime) > 500) {//比较两次按键时间差
-            Toast.makeText(this, "再按一次退出程序", Toast.LENGTH_SHORT).show();
-            mPressedTime = mNowTime;
-        } else {//退出程序
-            this.finish();
-            System.exit(0);
-        }
-    }
+//    @Override
+//    public void onBackPressed() {
+//        long mNowTime = System.currentTimeMillis();//获取第一次按键时间
+//        if ((mNowTime - mPressedTime) > 1000) {//比较两次按键时间差
+//            Toast.makeText(this, "再按一次退出程序", Toast.LENGTH_SHORT).show();
+//            mPressedTime = mNowTime;
+//        } else {//退出程序
+//            this.finish();
+////            System.exit(0);
+//        }
+//    }
 
 
     private void updatePixelFormatView(final Integer pixelFormat, final Integer w, final Integer h) {
